e509cd97e7f3e34d4419379c7e6b9e7e70c0fca3
eduardodelreguero <ereguero@gmail.com>
2018-01-26 18:51:47 +0000

    materialize

--- js/marvel_example.js
+++ js/marvel_example.js
@@ -7,9 +7,6 @@ var IMAGE_NOT_AVAIL = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_ava
 const API_KEY = "3ab1a7ad6985002c87082ccfee891dd9";
 const PRIV_KEY = "f92001eb96c00a07b6d9f66d1b108a8ec51d51ab";
 
-// Public Key
-// fc0e2b2bcfc273962ce941db43aa2039
-
 $(function(){
     var marvelAPI = 'https://gateway.marvel.com/v1/public/comics';
     $.getJSON( marvelAPI, {
@@ -18,12 +15,12 @@ $(function(){
         .done(function( response ) {
             let results = response.data.results;
             let resultsSize = results.length;
-            let output = '<ul>'; 
+            let output = '<ul class="collection">'; 
           
             for(let i=0; i<resultsSize; i++){
                 if(results[i].images.length > 0) {
                     var imgPath = results[i].images[0].path + '/standard_xlarge.' + results[i].images[0].extension;
-                    output += '<li><img src="' + imgPath + '"><br>'+results[i].title+'</li>';
+                    output += '<li class="collection-item avatar"><img src="' + imgPath + '"><br>'+results[i].title+'</li>';
                 }
             }  
             output += '</ul>'
